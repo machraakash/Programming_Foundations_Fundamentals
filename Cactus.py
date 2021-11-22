669e273b19b4a226bcc9dba7e76a937db3bc53b3
machraakash <34833061+machraakash@users.noreply.github.com>
2021-11-22 19:15:15 +0530

    Add files via upload

---
+++ Cactus.py
@@ -0,0 +1,6 @@
+plant = "Cacti"
+
+if plant == "Cacti":
+    print(plant,", This plant needs a lot of water")
+else:
+    print(plant,", Does not need a lot of water")
---
+++ Kihaal.py
@@ -0,0 +1,5 @@
+print("Ke Haal bhai?")
+salutation = input ("Sab vadia?")
+
+if salutation =="Thik bhai, tera ki haal je?":
+    print("Kujh ni, padosi saara din kutti-chika layi rakhde ne.")
